e29180cd7673df6511a1a43d39edb995ae840a20
akdsco <arkadiusz@ostrowski.co>
2019-04-14 11:36:52 +0100

    add arrays with data

---
+++ js/quote-generator.js
@@ -0,0 +1,14 @@
+let lifeQuoteSentences =
+    [
+        ["Marvelous objects disappear","Love never dies", "Money comes by"],
+        ["within an hour", "easily and with care", "when you don't want it"],
+        [", that's why you need all of it.",", consider yourself prepared for it."," so stay calm and watch."]
+    ];
+
+let lawsQuoteSentences =
+    [
+        ["Gold's Law:" , "Weinberg's Second Law:" , "Murphy's Fail Law:", "Visionary Law:"],
+        ["Often expressed as a relative priority" , "Every solution", "The legibility of a copy", "A falling object"],
+        [" is directly proportional to the cost of the item." , " breeds new problems." , " is inversely proportional to its importance.", "will always land where it can do the most damage."]
+    ];
+
